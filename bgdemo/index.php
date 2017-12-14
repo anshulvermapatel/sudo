39ed610245d7060a65f2720af6ca858b6b5d2325
Anshul Verma <anshulvermapatel@gmail.com>
2017-12-14 19:09:27 +0530

    same

--- bgdemo/index.php
+++ bgdemo/index.php
@@ -4,7 +4,7 @@
 <html>
 <!--<![endif]-->
   <head>
-    <title>Basic - Anshul Verma | Openshift Team</title>
+    <title>Basic - Anshul Verma | Openshift</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../dist/img/favicon.ico">
     <!-- iPad retina icon -->
